9aff069845ddcab7a1ca29ef326fe60e0d8fe28f
GBbartosz <bartosz.grygalewicz@gmail.com>
2024-01-20 14:10:12 +0100

    Initial commit

---
+++ download_data.py
@@ -0,0 +1,83 @@
+import datetime
+import pandas as pd
+import requests
+import sys
+import yfinance as yf
+
+
+def pandas_df_display_options():
+    pd.reset_option('display.max_rows')
+    pd.reset_option('display.max_columns')
+    pd.reset_option('display.width')
+    pd.reset_option('display.float_format')
+    pd.reset_option('display.max_colwidth')
+
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.max_colwidth', 20)
+    pd.set_option('display.width', 400)
+
+
+def get_update_notebook():
+    # gets only records which haven't benn updated today already
+    # updates first the oldest records
+    global today
+    df = pd.read_excel('C:\\Users\\barto\\Desktop\\Inwestor_2024\\update_notebook.xlsx', index_col=0)
+    df = df.sort_values(by='last_update_date', ascending=True)
+    df = df[df['last_update_date'] != today]
+    return df
+
+
+def download_financial_statements(ticker):
+    # if function in 'INCOME_STATEMENT', 'BALANCE_SHEET', 'CASH_FLOW' creates new file or adds new data to existing files
+    # if function in 'OVERVIEW' always creates new file
+
+    def api_limit(mydata):
+        # ends execution iif api limit is reached
+        global update_df
+        if 'Information' in mydata.keys():
+            update_df.to_excel('C:\\Users\\barto\\Desktop\\Inwestor_2024\\update_notebook.xlsx')
+            print('You reached api limit (25 per day)!')
+            sys.exit()
+
+    apikey = 'KMTF2LUFBCUIXYQU'
+    functions = ['OVERVIEW', 'INCOME_STATEMENT', 'BALANCE_SHEET', 'CASH_FLOW']
+    for function in functions:
+        file_path = f'C:\\Users\\barto\\Desktop\\Inwestor_2024\\financial_statements\\{ticker}_{function}.csv'
+        url = f'https://www.alphavantage.co/query?function={function}&symbol={ticker}&apikey={apikey}'
+        r = requests.get(url)
+        data = r.json()
+        api_limit(data)
+        if function in ['INCOME_STATEMENT', 'BALANCE_SHEET', 'CASH_FLOW']:
+            new_df = pd.DataFrame(data['quarterlyReports']).set_index('fiscalDateEnding')
+            try:
+                old_df = pd.read_csv(file_path, index_col=0)
+                mask = ~new_df.index.isin(old_df.index)
+                df = pd.concat([new_df[mask], old_df])
+            except FileNotFoundError:
+                df = new_df
+            df = df.sort_index(ascending=False)
+        else:
+            df = pd.DataFrame(data=data.values(), columns=['data'], index=data.keys())
+        df.to_csv(file_path)
+
+
+def download_price(ticker):
+    # always creates new file
+    price_df = yf.download(ticker, start='2018-01-01', end=datetime.datetime.today().date())
+    price_df = price_df.sort_index(ascending=False)
+    price_df.to_csv(f'C:\\Users\\barto\\Desktop\\Inwestor_2024\\price\\{ticker}_price.csv')
+
+
+pandas_df_display_options()
+today = datetime.datetime.today().date()
+update_df = get_update_notebook()
+
+for ticker in update_df.index:
+    print(ticker)
+    download_financial_statements(ticker)
+    download_price(ticker)
+    update_df.loc[ticker, 'last_update_date'] = today
+    print('Success!')
+
+update_df.to_excel('C:\\Users\\barto\\Desktop\\Inwestor_2024\\update_notebook.xlsx')
